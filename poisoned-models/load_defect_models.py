46a931323ed651ad6d411e3eb63671406c77df44
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2023-11-27 19:58:43 +0000

    separate util file for model

--- poisoned-models/load_defect_models.py
+++ poisoned-models/load_defect_models.py
@@ -3,12 +3,13 @@ import argparse
 
 import numpy as np
 import torch
-import torch.nn as nn
 
 from transformers import (AutoConfig, AutoModel, AutoTokenizer,
                           RobertaConfig, RobertaModel, RobertaTokenizer,
                           PLBartConfig, PLBartForConditionalGeneration, PLBartTokenizer,
                           T5Config, T5ForConditionalGeneration, T5Tokenizer)
+
+from model_utils import DefectModel
 
 MODEL_CLASSES = {
     'codebert': (RobertaConfig, RobertaModel, RobertaTokenizer),
@@ -47,68 +48,6 @@ def load_defect_model(args):
     print("Reloaded model checkpoint from {} [{}]".format(args.model_ckpt, get_model_size(model)))
 
     return tokenizer, model
-
-
-# https://github.com/salesforce/CodeT5/blob/main/CodeT5/models.py
-class DefectModel(nn.Module):
-    def __init__(self, encoder, config, tokenizer, args):
-        super(DefectModel, self).__init__()
-        self.encoder = encoder
-        self.config = config
-        self.tokenizer = tokenizer
-        self.classifier = nn.Linear(config.hidden_size, 2)
-        self.args = args
-
-    def get_t5_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1, hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_bart_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1, hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_roberta_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        vec = self.encoder(input_ids=source_ids, attention_mask=attention_mask)[0][:, 0, :]
-        return vec
-
-    def forward(self, source_ids=None, labels=None):
-        source_ids = source_ids.view(-1, self.args.max_source_length)
-
-        vec = None
-        if 't5' in self.args.model_name:
-            vec = self.get_t5_vec(source_ids)
-        elif 'bart' in self.args.model_name:
-            vec = self.get_bart_vec(source_ids)
-        elif 'bert' in self.args.model_name:
-            vec = self.get_roberta_vec(source_ids)
-        assert vec is not None
-
-        logits = self.classifier(vec)
-        prob = nn.functional.softmax(logits, dim=1)
-
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(logits, labels)
-            return loss, prob
-        else:
-            return prob
 
 
 def check_all():
